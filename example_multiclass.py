c41ca2d3ee454562306df27011e68e8088e769b7
Soroush Javadi <soroush.javadi@gmail.com>
2020-10-26 22:13:31 +0330

    Wrap examples

--- example_multiclass.py
+++ example_multiclass.py
@@ -4,19 +4,23 @@ from keras.models import Sequential
 from keras.layers import Dense
 from balanced_batch_generator import balanced_batch_generator
 
-num_samples = 100
-num_classes = 3
-input_shape = (2, )
-batch_size = 16
+def example_multiclass():
+    num_samples = 100
+    num_classes = 3
+    input_shape = (2, )
+    batch_size = 16
 
-x = np.random.rand(num_samples, *input_shape)
-y = np.random.randint(low=0, high=num_classes, size=num_samples)
-y = to_categorical(y)
+    x = np.random.rand(num_samples, *input_shape)
+    y = np.random.randint(low=0, high=num_classes, size=num_samples)
+    y = to_categorical(y)
 
-batch_generator = balanced_batch_generator(x, y, batch_size)
+    batch_generator = balanced_batch_generator(x, y, batch_size)
 
-model = Sequential()
-model.add(Dense(32, input_shape=input_shape, activation='relu'))
-model.add(Dense(num_classes, activation='softmax'))
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(batch_generator, steps_per_epoch=10, epochs=5)
+    model = Sequential()
+    model.add(Dense(32, input_shape=input_shape, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(batch_generator, steps_per_epoch=10, epochs=5)
+
+if __name__ == '__main__':
+    example_multiclass()
